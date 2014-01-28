c532f74ccabc5f5cbae5d0e97d1ca7ce714d27a9
mrong <gizmokaracho@gmx.de>
2014-01-28 09:34:18 +0100

    - basic structure

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :crm do
+  name 'Crm plugin'
+  author 'Mathias Rong'
+  description 'A Plugin for managing customers in Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
