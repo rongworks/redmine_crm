ac0abb1aeca93be23dc3d709a86d16d00862a203
rongworks <mathiasrong@gmail.com>
2014-05-26 10:41:56 +0200

    fixtures

--- test/unit/client_test.rb
+++ test/unit/client_test.rb
@@ -2,8 +2,27 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class ClientTest < ActiveSupport::TestCase
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  test 'should fail' do
+    assert false
   end
+
+  test 'should save valid client' do
+    client = clients(:valid_client)
+    assert  client.save
+  end
+
+  test 'should not save client without last_name' do
+    client = clients(:client_no_last_name)
+    assert_not client.save
+  end
+
+  test 'should be commentable' do
+    client = clients(:client_no_last_name)
+    client.save
+    comment = crmcomments(:valid_comment)
+    assert comment.save, "comment should have been saved"
+    assert client.crmcomments.include?(comment), "Client should reference new comment"
+  end
+
 end
