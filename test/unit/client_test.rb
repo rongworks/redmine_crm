18478257258e56064a8e9fcf7d18f4d1c78e69b5
mrong <mathiasrong@gmail.com>
2014-05-27 12:46:34 +0200

    possible workaround for fixtures

--- test/unit/client_test.rb
+++ test/unit/client_test.rb
@@ -2,7 +2,7 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class ClientTest < ActiveSupport::TestCase
 
-  fixtures :clients
+  plugin_fixtures :clients
 
   test 'should fail' do
     assert false
