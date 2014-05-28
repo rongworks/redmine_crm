37613d1f61a3e2e6c7b6acb67b0df2492a6b971b
mrong <mathiasrong@gmail.com>
2014-05-28 11:34:13 +0200

    fixed conflict with factory_girl gem, adjusted factory paths

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,23 +1,6 @@
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+require 'factory_girl_rails'
+FactoryGirl.definition_file_paths << File.expand_path('../factories', __FILE__)
+FactoryGirl.find_definitions
 include FactoryGirl::Syntax::Methods
-
-module Redmine
-  module PluginFixturesLoader
-    def self.included(base)
-      base.class_eval do
-        def self.plugin_fixtures(*symbols)
-          ActiveRecord::Fixtures.create_fixtures(File.dirname(__FILE__) + '/fixtures/', symbols)
-        end
-      end
-    end
-  end
-end
-
-unless ActionController::TestCase.included_modules.include?(Redmine::PluginFixturesLoader)
-  ActionController::TestCase.send :include, Redmine::PluginFixturesLoader
-end
-
-unless ActiveSupport::TestCase.included_modules.include?(Redmine::PluginFixturesLoader)
-  ActiveSupport::TestCase.send :include, Redmine::PluginFixturesLoader
-end
