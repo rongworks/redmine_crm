25c6d4cb7a1a6ca77d39177f2921fbb8dfcb0f72
mrong <mathiasrong@gmail.com>
2014-06-05 14:15:13 +0200

    fixed project relations

--- lib/projects_relation_patch.rb
+++ lib/projects_relation_patch.rb
@@ -2,16 +2,30 @@ require_dependency 'project'
 
 module ProjectsRelationPatch
   def self.included(base) # :nodoc:
+    # Add module to Project
+    base.extend(ClassMethods)
+
+    base.send(:include, InstanceMethods)
 
     # Same as typing in the class
+    #Add relations
     base.class_eval do
-      has_many :companies_projects
+      has_many :companies_projects, :class_name => 'CompaniesProjects', :foreign_key => 'project_id'
       has_many :companies, :through => :companies_projects
+      has_many :crm_actions, :through => :companies
+      has_many :clients, :through => :companies
     end
 
   end
 
+  module ClassMethods
+
+  end
+
+  module InstanceMethods
+
+  end
 end
 
-# Add module to Issue
 Project.send(:include, ProjectsRelationPatch)
+
