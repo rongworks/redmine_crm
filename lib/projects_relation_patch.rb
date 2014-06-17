a389ce9f46ef75992b2848156c238fa342f76187
mrong <mathiasrong@gmail.com>
2014-06-17 13:01:53 +0200

    make collections unique

--- lib/projects_relation_patch.rb
+++ lib/projects_relation_patch.rb
@@ -12,8 +12,8 @@ module ProjectsRelationPatch
     base.class_eval do
       has_many :companies_projects, :class_name => 'CompaniesProjects', :foreign_key => 'project_id'
       has_many :companies, :through => :companies_projects
-      has_many :crm_actions, :through => :companies
-      has_many :clients, :through => :companies
+      has_many :crm_actions, :through => :companies, :uniq => true
+      has_many :clients, :through => :companies, :uniq => true
     end
 
   end
